2c0d7319a5c999f23407f91b30f08303435fcf9c
Mehrshad <mehrshadhamavandy@gmail.com>
2025-02-03 11:13:27 -0800

    Vercel Fixed

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'), // Changed from 'build' to 'dist'
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   module: {
